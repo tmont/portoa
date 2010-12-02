4b51e9e6790c419bc93e843a943696c9ab44648a
Tommy Montgomery <tommy.mont@gmail.com>
2010-12-02 23:17:19 +0000

    added some matching rules and the LogAllMethodCalls unity extension

---
+++ Src/Portoa.Web/Unity/AlwaysApply.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace Portoa.Web.Unity {
+	public class AlwaysApply : IMatchingRule {
+		public bool Matches(MethodBase member) {
+			return true;
+		}
+	}
+}
---
+++ Src/Portoa.Web/Unity/LogAllMethodCalls.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using Portoa.Logging;
+
+namespace Portoa.Web.Unity {
+	public class LogAllMethodCalls : UnityContainerExtension {
+		protected override void Initialize() {
+			Container
+				.AddExtensionOnce<Interception>()
+				.Configure<Interception>()
+				.AddPolicy("LoggingPolicy")
+				.AddCallHandler<LoggerCallHandler>()
+				.AddMatchingRule<AlwaysApply>()
+				.AddMatchingRule<NotInstanceOf<ILogger>>();
+		}
+	}
+}
