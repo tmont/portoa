f18889af7b37112d7ef6fd10495f68a361fb0d93
Tommy Montgomery <tommy.mont@gmail.com>
2010-12-06 06:59:20 +0000

    comments

--- Src/Portoa.Web/Controllers/IErrorController.cs
+++ Src/Portoa.Web/Controllers/IErrorController.cs
@@ -1,9 +1,22 @@
 ﻿using System.Web.Mvc;
 
 namespace Portoa.Web.Controllers {
+	/// <summary>
+	/// Signifies that this controller can handle errors that aren't handled by
+	/// application code
+	/// </summary>
 	public interface IErrorController : IController {
+		/// <summary>
+		/// Executed when an unknown error occurs
+		/// </summary>
 		ActionResult Unknown();
+		/// <summary>
+		/// Executed when a page is not found
+		/// </summary>
 		ActionResult NotFound();
+		/// <summary>
+		/// Executed when the user is forbidden from seeing the requested page
+		/// </summary>
 		ActionResult Forbidden();
 	}
 }
