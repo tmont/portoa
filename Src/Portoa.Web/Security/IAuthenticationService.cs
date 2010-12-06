0d754d281453180d40f69809f1f2e6c3068583fc
Tommy Montgomery <tommy.mont@gmail.com>
2010-12-06 07:25:24 +0000

    changed signature of IAuthenticationService.Login()

--- Src/Portoa.Web/Security/IAuthenticationService.cs
+++ Src/Portoa.Web/Security/IAuthenticationService.cs
@@ -1,9 +1,24 @@
 ﻿using Portoa.Logging;
 
 namespace Portoa.Web.Security {
+	/// <summary>
+	/// Service for authenticating users
+	/// </summary>
 	public interface IAuthenticationService {
-		void Login(string username, bool createPersistentCookie);
+		/// <summary>
+		/// Logs in the specified user with the specified username
+		/// </summary>
+		void Login(string username);
+
+		/// <summary>
+		/// Logs out the currently logged in user
+		/// </summary>
 		void Logout();
+
+		/// <summary>
+		/// Determines if a user identified by the given username and password
+		/// is a valid user in the system
+		/// </summary>
 		bool IsValid(string username, [DoNotLog]string password);
 	}
 }
