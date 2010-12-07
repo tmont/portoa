2b57fa561e3650876819351531dfef2af776f8da
Tommy Montgomery <tommy.mont@gmail.com>
2010-12-07 23:35:49 +0000

    added Walk and WalkDeferred extension methods

--- Src/Portoa/Util/LinqExtensions.cs
+++ Src/Portoa/Util/LinqExtensions.cs
@@ -10,5 +10,28 @@ namespace Portoa.Util {
 		public static string Implode<T>(this IEnumerable<T> source, Func<T, string> selector, string separator) {
 			return string.Join(separator, source.Select(selector).ToArray());
 		}
+
+		/// <summary>
+		/// Performs the specified action on each item in the collection. The action will execute immediately.
+		/// Use <c cref="WalkDeferred{T}">WalkDeferred</c> if deferred execution is needed.
+		/// </summary>
+		/// <seealso cref="WalkDeferred{T}"/>
+		public static void Walk<T>(this IEnumerable<T> collection, Action<T> action) {
+			foreach (var item in collection) {
+				action(item);
+			}
+		}
+
+		/// <summary>
+		/// Performs the specified action on each item in the collection. The action will not execute until the collection
+		/// is enumerated. Use <c cref="Walk{T}">Walk</c> if deferred execution is not needed.
+		/// </summary>
+		/// <seealso cref="Walk{T}"/>
+		public static IEnumerable<T> WalkDeferred<T>(this IEnumerable<T> collection, Action<T> action) {
+			foreach (var item in collection) {
+				action(item);
+				yield return item;
+			}
+		}
 	}
 }
