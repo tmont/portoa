b7cd160ab0dc75e5087751904ee9880913a2481c
Tommy Montgomery <tommy.mont@gmail.com>
2011-02-26 01:14:31 +0000

    added ISearchable interface

---
+++ Src/Portoa/Search/ISearchable.cs
@@ -0,0 +1,12 @@
+﻿namespace Portoa.Search {
+	/// <summary>
+	/// Indicates that this object is searchable
+	/// </summary>
+	public interface ISearchable {
+		/// <summary>
+		/// Gets whether or not the index for this object should be updated
+		/// (a dirty flag, of sorts)
+		/// </summary>
+		bool ShouldIndex { get; }
+	}
+}
