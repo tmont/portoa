26a73d49e05a643aa7b5a77bea501cfeeb77bd36
Tommy Montgomery <tommy.mont@gmail.com>
2011-03-07 16:19:44 +0000

    added forms auth base service

---
+++ Src/Portoa.Web/Security/FormsAuthenticationServiceBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Security;
+
+namespace Portoa.Web.Security {
+	/// <summary>
+	/// Base class for authenticating users using the built-in <see cref="FormsAuthentication"/>
+	/// mechanism
+	/// </summary>
+	public abstract class FormsAuthenticationServiceBase : IAuthenticationService {
+		void IAuthenticationService.Login(string username) {
+			FormsAuthentication.SetAuthCookie(username, true);
+			Login(username);
+		}
+
+		void IAuthenticationService.Logout() {
+			FormsAuthentication.SignOut();
+			Logout();
+		}
+
+		/// <summary>
+		/// Performs any extra logout tasks; default implementation does nothing
+		/// </summary>
+		protected virtual void Logout() { }
+
+		/// <summary>
+		/// Performs any extra login tasks; default implementation does nothing
+		/// </summary>
+		/// <param name="username">The name of the user to login</param>
+		protected virtual void Login(string username) { }
+
+		public abstract bool IsValid(string username, string password);
+	}
+}
