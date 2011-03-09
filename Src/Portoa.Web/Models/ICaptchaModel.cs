a74390eefadd49d22436c26830c29733b84c6dc3
Tommy Montgomery <tommy.mont@gmail.com>
2011-03-09 00:10:29 +0000

    added ICaptchaModel

---
+++ Src/Portoa.Web/Models/ICaptchaModel.cs
@@ -0,0 +1,24 @@
+﻿using Portoa.Security;
+
+namespace Portoa.Web.Models {
+	/// <summary>
+	/// Represents a model to be used to verify that the user is not a spambot
+	/// </summary>
+	public interface ICaptchaModel {
+		/// <summary>
+		/// The answer to the captcha question supplied by the user
+		/// </summary>
+		string CaptchaAnswer { get; set; }
+		/// <summary>
+		/// The hashed version of the captcha answer
+		/// </summary>
+		/// <see cref="CaptchaManager.IsMatch"/>
+		string HashedCaptchaAnswer { get; set; }
+		/// <summary>
+		/// The unhashed, plaintext version of the captcha answer, to be sent from the server
+		/// to the client to construct an appropriate view (this value should not be sent back
+		/// from the client, obviously)
+		/// </summary>
+		string UnhashedCaptchaAnswer { get; set; }
+	}
+}
