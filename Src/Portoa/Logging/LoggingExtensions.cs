4019fdb0e66ee12276b378640c3d60e221674771
Tommy Montgomery <tommy.mont@gmail.com>
2011-03-14 18:53:53 +0000

    added a test, deleted a file

--- Src/Portoa/Logging/LoggingExtensions.cs
+++ Src/Portoa/Logging/LoggingExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using Portoa.Util;
@@ -13,5 +15,37 @@ namespace Portoa.Logging {
 			return !attributeProvider.GetAttributes<DoNotLogAttribute>().Any();
 		}
 
+		/// <summary>
+		/// Logs a formatted <c>Debug</c> message
+		/// </summary>
+		/// <see cref="ILogger.Debug"/>
+		public static void Debug(this ILogger logger, string messageFormat, params object[] args) {
+			logger.Debug(string.Format(CultureInfo.InvariantCulture, messageFormat, args));
+		}
+
+		/// <summary>
+		/// Logs a formatted <c>Info</c> message
+		/// </summary>
+		/// <see cref="ILogger.Info"/>
+		public static void Info(this ILogger logger, string message, params object[] args) {
+			logger.Info(string.Format(CultureInfo.InvariantCulture, message, args));
+		}
+
+		/// <summary>
+		/// Logs a formatted <c>Warn</c> message
+		/// </summary>
+		/// <see cref="ILogger.Warn"/>
+		public static void Warn(this ILogger logger, string message, params object[] args) {
+			logger.Warn(string.Format(CultureInfo.InvariantCulture, message, args));
+		}
+
+		/// <summary>
+		/// Logs an exception using <see cref="ILogger.Debug"/>
+		/// </summary>
+		/// <see cref="ILogger.Debug"/>
+		public static void Exception(this ILogger logger, Exception e) {
+			logger.Debug(e.Message);
+			logger.Debug(e.StackTrace);
+		}
 	}
 }
---
+++ Tests/Portoa.Tests/Persistence/EntityNotFoundTests.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using NUnit.Framework;
+using Portoa.Persistence;
+
+namespace Portoa.Tests.Persistence {
+	[TestFixture]
+	public class EntityNotFoundTests {
+		[Test]
+		public void Should_properly_format_exception_message() {
+			var exception = new EntityNotFoundException<MyEntity, int>(9);
+			Assert.That(exception.Message, Is.EqualTo("Entity of type MyEntity not found with ID 9"));
+		}
+
+		[UsedImplicitly]
+		private class MyEntity : Entity<int> { }
+	}
+}
