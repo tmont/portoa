2a8e800e060025ebc20015fb8b6844ed393bcb88
Tommy Montgomery <tommy.mont@gmail.com>
2011-03-17 18:41:04 +0000

    updated ParserHelper

--- Src/Portoa.Web/Rest/ParserHelper.cs
+++ Src/Portoa.Web/Rest/ParserHelper.cs
@@ -12,7 +12,7 @@ namespace Portoa.Web.Rest {
 		/// <param name="unparsedCriteria">The value to parse</param>
 		public static CriterionSet Parse(string unparsedCriteria) {
 			var tokens = new CommonTokenStream(new CriterionLexer(new ANTLRStringStream(unparsedCriteria)));
-			return new CriterionParser(tokens) { Builder = new CriteriaSetBuilder() }.getCriteria();
+			return new CriterionParser(tokens) { Builder = new CriterionBuilder() }.getCriteria();
 		}
 	}
 }
